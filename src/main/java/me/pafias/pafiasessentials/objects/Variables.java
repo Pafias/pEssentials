9b5dfcf1c1454f08572d72c2118dd269a003a542
Pafias <29665404+Pafias@users.noreply.github.com>
2023-01-21 14:48:10 +0100

    added /discord

--- src/main/java/me/pafias/pafiasessentials/objects/Variables.java
+++ src/main/java/me/pafias/pafiasessentials/objects/Variables.java
@@ -17,15 +17,14 @@ public class Variables {
 
     // config.yml
     public String hubServer = "hub";
-    public String staffchatFormat = "[Staff] {player}: {message}";
-    public boolean antilog4j = true;
+    public String staffchatFormat = "&e&l[STAFF] &r&9{player}&r: {message}";
     public boolean hidePlugins = true;
 
     public boolean joinMessage = true;
     public boolean quitMessage = true;
     public boolean highPingKick = false;
     public int pingKickThreshold = 150;
-    public String rickrollResourcePack;
+    public String discordLink;
 
     private void reloadConfigYML() {
         plugin.getConfig().options().copyDefaults(true);
@@ -33,13 +32,12 @@ public class Variables {
         plugin.reloadConfig();
         hubServer = plugin.getConfig().getString("hub_server");
         staffchatFormat = plugin.getConfig().getString("staffchat_format");
-        antilog4j = plugin.getConfig().getBoolean("log4j_exploit_protection");
         hidePlugins = plugin.getConfig().getBoolean("hide_plugins");
         joinMessage = plugin.getConfig().getBoolean("join_message_enabled");
         quitMessage = plugin.getConfig().getBoolean("quit_message_enabled");
         highPingKick = plugin.getConfig().getBoolean("kick_on_high_ping");
         pingKickThreshold = plugin.getConfig().getInt("ping_kick_threshold");
-        rickrollResourcePack = plugin.getConfig().getString("rickroll_resourcepack_link");
+        discordLink = plugin.getConfig().getString("discord_link");
     }
 
 }
