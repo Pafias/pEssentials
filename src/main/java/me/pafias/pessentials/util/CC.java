9c821eb89b01a09baa59287effa4d35076c5b985
Pafias <29665404+Pafias@users.noreply.github.com>
2023-09-11 10:47:32 +0200

    added hex color support + some older changes i didn't commit lol

--- src/main/java/me/pafias/pessentials/util/CC.java
+++ src/main/java/me/pafias/pessentials/util/CC.java
@@ -1,6 +1,9 @@
 package me.pafias.pessentials.util;
 
 import me.pafias.pessentials.pEssentials;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.bukkit.ChatColor;
 
 import java.util.List;
@@ -8,16 +11,38 @@ import java.util.stream.Collectors;
 
 public class CC {
 
-    public static String t(String string) {
-        return ChatColor.translateAlternateColorCodes('&', string);
+    public static TextComponent a(String s) {
+        if (s == null) return null;
+        return LegacyComponentSerializer.legacyAmpersand().deserialize(s);
+    }
+
+    public static TextComponent af(String s, Object... o) {
+        if (s == null) return null;
+        return a(String.format(s, o));
+    }
+
+    public static List<Component> a(List<String> list) {
+        if (list == null) return null;
+        return list.stream().map(CC::a).collect(Collectors.toList());
+    }
+
+    public static String t(String s) {
+        if (s == null) return null;
+        return ChatColor.translateAlternateColorCodes('&', s);
     }
 
     public static String tf(String s, Object... o) {
+        if (s == null) return null;
         return t(String.format(s, o));
     }
 
     public static List<String> t(List<String> list) {
+        if (list == null) return null;
         return list.stream().map(CC::t).collect(Collectors.toList());
+    }
+
+    public static TextComponent formatStaffchatModern(String name, String message) {
+        return a(pEssentials.get().getSM().getVariables().staffchatFormat.replace("{player}", name).replace("{message}", message));
     }
 
     public static String formatStaffchat(String name, String message) {
