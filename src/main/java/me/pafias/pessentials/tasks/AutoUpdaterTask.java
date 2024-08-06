e8236e5135f16313517a921793db5d63e1540bc8
Pafias <29665404+Pafias@users.noreply.github.com>
2024-08-06 12:41:10 +0100

    fixed auto updater

--- src/main/java/me/pafias/pessentials/tasks/AutoUpdaterTask.java
+++ src/main/java/me/pafias/pessentials/tasks/AutoUpdaterTask.java
@@ -1,12 +1,12 @@
 package me.pafias.pessentials.tasks;
 
 import me.pafias.pessentials.pEssentials;
-import me.pafias.pessentials.util.Tasks;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.protocol.BasicHttpContext;
+import org.bukkit.Bukkit;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import java.io.BufferedInputStream;
@@ -14,6 +14,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URI;
+import java.nio.file.Files;
 
 public class AutoUpdaterTask extends BukkitRunnable {
 
@@ -23,33 +24,48 @@ public class AutoUpdaterTask extends BukkitRunnable {
         this.plugin = plugin;
     }
 
+    @Override
     public void run() {
         try {
             CloseableHttpClient closeableHttpClient = HttpClientBuilder.create().build();
             HttpGet httpGet = new HttpGet();
             httpGet.setURI(new URI("https://pafias.me/minecraft/pEssentials/pEssentials.jar"));
             HttpResponse httpResponse = closeableHttpClient.execute(httpGet, new BasicHttpContext());
+
+            File tempJarFile = new File(plugin.getDataFolder(), "pEssentials_update.jar");
             File jarFileOnPluginFolder = new File(plugin.getClass().getProtectionDomain().getCodeSource().getLocation().toURI());
+
             if (jarFileOnPluginFolder.length() != httpResponse.getEntity().getContentLength()) {
-                FileOutputStream fileOutputStream = new FileOutputStream(jarFileOnPluginFolder);
-                BufferedInputStream bufferedInputStream = null;
-                try {
-                    plugin.getLogger().info("Found a new version and downloading the new update!");
-                    bufferedInputStream = new BufferedInputStream(httpResponse.getEntity().getContent());
-                    byte[] bytes = new byte[1024];
-                    int bytesCounter;
-                    while ((bytesCounter = bufferedInputStream.read(bytes, 0, 1024)) != -1)
-                        fileOutputStream.write(bytes, 0, bytesCounter);
-                    plugin.getLogger().info("Updated to latest version successfully. Restarting in 5 seconds...");
-                    Tasks.runLaterSync(5 * 20, () -> plugin.getServer().shutdown());
-                } finally {
-                    httpResponse.getEntity().getContent().close();
-                    fileOutputStream.close();
-                    if (bufferedInputStream != null)
-                        bufferedInputStream.close();
+                plugin.getLogger().info("Found a new version and downloading the update!");
+
+                try (BufferedInputStream in = new BufferedInputStream(httpResponse.getEntity().getContent());
+                     FileOutputStream out = new FileOutputStream(tempJarFile)) {
+
+                    byte[] buffer = new byte[1024];
+                    int count;
+                    while ((count = in.read(buffer)) != -1) {
+                        out.write(buffer, 0, count);
+                    }
+                    plugin.getLogger().info("Downloaded update successfully.");
+
+                    plugin.getServer().getScheduler().runTaskLater(plugin, () -> {
+                        plugin.getLogger().info("Server restarting to apply update...");
+                        Bukkit.getPluginManager().disablePlugins();
+
+                        try {
+                            Files.move(tempJarFile.toPath(), jarFileOnPluginFolder.toPath(), java.nio.file.StandardCopyOption.REPLACE_EXISTING);
+                            plugin.getLogger().info("Updated to the latest version successfully. Restarting server...");
+                        } catch (IOException e) {
+                            plugin.getLogger().severe("Failed to replace the old JAR file with the new one.");
+                            e.printStackTrace();
+                        }
+
+                        Bukkit.shutdown();
+                    }, 40);
                 }
             }
         } catch (IOException | java.net.URISyntaxException ex) {
+            plugin.getLogger().severe("Failed to check for updates.");
             ex.printStackTrace();
         }
     }
