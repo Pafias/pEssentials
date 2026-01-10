228e126b085c4484976f51aaf2f857ebe81c7dc9
Pafias <29665404+Pafias@users.noreply.github.com>
2026-01-10 17:44:08 +0100

    fix a warning, and add more listeners to the sitlistener

--- pEssentials/src/main/java/me/pafias/pessentials/listeners/SitListener.java
+++ pEssentials/src/main/java/me/pafias/pessentials/listeners/SitListener.java
@@ -5,6 +5,8 @@ import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.HumanEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.spigotmc.event.entity.EntityDismountEvent;
 
 public class SitListener implements Listener {
@@ -16,6 +18,23 @@ public class SitListener implements Listener {
         if (as == null || event.getDismounted() != as) return;
         as.remove();
         SitCommand.map.remove(human);
+    }
+
+    @EventHandler
+    public void onDeath(EntityDeathEvent event) {
+        if (!event.getEntity().isInsideVehicle()) return;
+        if (!(event.getEntity() instanceof HumanEntity human)) return;
+        final ArmorStand as = SitCommand.map.remove(human);
+        if (as == null) return;
+        as.remove();
+    }
+
+    @EventHandler
+    public void onQuit(PlayerQuitEvent event) {
+        if (!event.getPlayer().isInsideVehicle()) return;
+        final ArmorStand as = SitCommand.map.remove(event.getPlayer());
+        if (as == null) return;
+        as.remove();
     }
 
 }
