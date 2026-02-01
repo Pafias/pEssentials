ba1c5f20b2ccff58d560ef0fd2c551af108a4f60
Pafias <29665404+Pafias@users.noreply.github.com>
2026-02-02 00:26:56 +0100

    a few fixes

--- pEssentials/src/main/java/me/pafias/pessentials/commands/modules/SudoCommand.java
+++ pEssentials/src/main/java/me/pafias/pessentials/commands/modules/SudoCommand.java
@@ -23,44 +23,42 @@ public class SudoCommand extends ICommand {
 
     @Override
     public void commandHandler(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        if (sender.isOp()) {
-            if (args.length < 2) {
-                sender.sendMessage(CC.t(String.format("&c/%s <player> <command>", label)));
-                return;
-            }
-            final Player target = plugin.getServer().getPlayer(args[0]);
-            if (target == null || (sender instanceof Player senderPlayer && !senderPlayer.canSee(target))) {
-                sender.sendMessage(CC.t("&cPlayer not found!"));
-                return;
-            }
-            final String[] argsParsed = Arrays.copyOfRange(args, 2, args.length);
-            if (args[1].equalsIgnoreCase("chat")) {
-                final String message = String.join(" ", argsParsed);
-                target.chat(message);
-            } else {
-                final PluginCommand cmd = plugin.getServer().getPluginCommand(args[1]);
-                if (cmd == null) {
-                    sender.sendMessage(CC.t("&cCommand not found!"));
-                    return;
-                }
-                final PermissionAttachment attachment = target.addAttachment(plugin);
-                boolean hasPerm = cmd.testPermissionSilent(target);
-                boolean removePerm;
-                if (!hasPerm && cmd.getPermission() != null) {
-                    attachment.setPermission(cmd.getPermission(), true);
-                    removePerm = true;
-                } else removePerm = false;
-                try {
-                    cmd.execute(target, args[1], argsParsed);
-                } catch (Throwable ignored) {
-                } finally {
-                    if (removePerm)
-                        attachment.unsetPermission(cmd.getPermission());
-                    target.removeAttachment(attachment);
-                }
-            }
-            sender.sendMessage(CC.t("&aSudo executed."));
+        if (args.length < 2) {
+            sender.sendMessage(CC.t(String.format("&c/%s <player> <command>", label)));
+            return;
         }
+        final Player target = plugin.getServer().getPlayer(args[0]);
+        if (target == null || (sender instanceof Player senderPlayer && !senderPlayer.canSee(target))) {
+            sender.sendMessage(CC.t("&cPlayer not found!"));
+            return;
+        }
+        final String[] argsParsed = Arrays.copyOfRange(args, 2, args.length);
+        if (args[1].equalsIgnoreCase("chat")) {
+            final String message = String.join(" ", argsParsed);
+            target.chat(message);
+        } else {
+            final PluginCommand cmd = plugin.getServer().getPluginCommand(args[1]);
+            if (cmd == null) {
+                sender.sendMessage(CC.t("&cCommand not found!"));
+                return;
+            }
+            final PermissionAttachment attachment = target.addAttachment(plugin);
+            boolean hasPerm = cmd.testPermissionSilent(target);
+            boolean removePerm;
+            if (!hasPerm && cmd.getPermission() != null) {
+                attachment.setPermission(cmd.getPermission(), true);
+                removePerm = true;
+            } else removePerm = false;
+            try {
+                cmd.execute(target, args[1], argsParsed);
+            } catch (Throwable ignored) {
+            } finally {
+                if (removePerm)
+                    attachment.unsetPermission(cmd.getPermission());
+                target.removeAttachment(attachment);
+            }
+        }
+        sender.sendMessage(CC.t("&aSudo executed."));
     }
 
     @Override
